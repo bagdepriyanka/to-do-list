2a647ea585a32c288a53ac65642531bbeb8d41a2
bagdepriyanka <bagde.priyanka108@gmail.com>
2021-03-27 00:09:41 +0530

    production database connection code

--- common_functions.php
+++ common_functions.php
@@ -2,11 +2,20 @@
 
 function connect()
 {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
+
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+
+    //for production
+    $servername = "sql106.epizy.com";
+    $username = "epiz_28188441";
+    $password = "Et7CNq7SEji";
+
+
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=todo_list", $username, $password);
+        // $conn = new PDO("mysql:host=$servername;dbname=todo_list", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=epiz_28188441_XXX", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
